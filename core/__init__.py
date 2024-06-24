976ecbb755e18111f1b8f2a2acea0b73adc10c1d
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-25 00:51:33 +0900

    импорты, библиотеки, ридми

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from core.config_reader import config as app_config
+from core.fastapi_app.schemes import *
+from core.fastapi_app.websocket_connection_manager import manager as websocket_manager
+# достучаться до этих импортов можно в любом файле внутри core
+# по импорту from core import *

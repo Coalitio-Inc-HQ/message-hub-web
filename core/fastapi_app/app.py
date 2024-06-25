feb272a4d84968e9ffe6536ceb87ccebf221c399
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-25 16:33:27 +0900

    Создал обработчик обращений через вебсокет
    добавил много новых схем, а также ActionsMapTypedDict, который содержим формат обращений, который я ожидаю из фронта. Здесь же можно увидеть, какие параметры необходимы
    
    Однако общение происходит моделями действия ActionDTO, где в body уже будут параметры, показанные в ActionsMapTypedDict

--- core/fastapi_app/app.py
+++ core/fastapi_app/app.py
@@ -1,70 +1,44 @@
-import asyncio
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-from core.database.database import async_session, create_db_and_tables
-from ..database import crud
-from core.database import schemas
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from starlette.websockets import WebSocketDisconnect, WebSocket
 
-app = FastAPI()
+from pydantic import parse_obj_as
 
-# @app.on_event("startup")
-# async def startup():
-#     await create_db_and_tables()
+from core.fastapi_app.front_client import send_front_waiting_chats_by_user
 
-# async def get_db():
-#     async with async_session() as session:
-#         yield session
+from core import *
 
-@app.post("/register_platform/")
-async def register_platform(url: str, platform_name: str):
-    await crud.copper_client.register_platform(url, platform_name)
-    return {"message": "Platform registered successfully"}
+actions_map = ActionsMapTypedDict(get_waiting_chats=send_front_waiting_chats_by_user)
 
-@app.post("/register_user/", response_model=schemas.User)
-async def register_user(platform_name: str, user_firstname: str, db: AsyncSession = Depends(get_db)):
-    user_id = await crud.copper_client.register_user(platform_name, user_firstname)
-    new_user = schemas.UserCreate(name=user_firstname)
-    user = await crud.create_user(db, new_user)
-    user.id = user_id  # Обновляем ID
-    return user
 
-@app.get("/waiting_chats/{user_id}", response_model=list[schemas.Chat])
-async def get_waiting_chats(user_id: int):
-    chats = await crud.get_waiting_chats(user_id)
-    if not chats:
-        raise HTTPException(status_code=404, detail="No waiting chats found")
-    return chats
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
 
-@app.post("/read_chat/", response_model=schemas.Chat)
-async def read_chat_by_user(user_id: int, chat_id: int):
-    chat = await crud.read_chat_by_user(user_id, chat_id)
-    if not chat:
-        raise HTTPException(status_code=404, detail="Chat not found")
-    return chat
 
-@app.post("/add_user_to_chat/")
-async def add_user_to_chat(user_id: int, chat_id: int):
-    await crud.add_user_to_chat(user_id, chat_id)
-    return {"message": "User added to chat"}
+app = FastAPI(lifespan=lifespan)
 
-@app.get("/user_chats/{user_id}", response_model=list[schemas.Chat])
-async def get_chats_by_user(user_id: int):
-    chats = await crud.get_chats_by_user(user_id)
-    if not chats:
-        raise HTTPException(status_code=404, detail="No chats found")
-    return chats
 
-@app.get("/messages/{chat_id}", response_model=list[schemas.Message])
-async def get_messages_from_chat(chat_id: int, count: int):
-    messages = await crud.get_messages_from_chat(chat_id, count)
-    if not messages:
-        raise HTTPException(status_code=404, detail="No messages found")
-    return messages
+@app.websocket(f"{app_config.WS_LISTENER_URL}")
+async def websocket_endpoint(websocket: WebSocket):  # в будущем авторизация по токену
+    """
+    :param websocket:
+    :return:
+    """
+    await websocket_manager.connect(websocket)
+    try:
+        while True:
+            # Получаем данные от фронта в формате ActionDTO
+            data = await websocket.receive_json()
+            action = parse_obj_as(ActionDTO, data)
+            if actions_map.__contains__(action.name):
+                await actions_map[action.name](action.body, websocket)
+            return
+    except WebSocketDisconnect:
+        websocket_manager.disconnect(websocket)
 
-@app.post("/messages/", response_model=schemas.Message)
-async def send_message_to_chat(message: schemas.MessageCreate):
-    return await crud.send_message_to_chat(message)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+    uvicorn.run('app:app', host="localhost", port=8000, reload=True)
--- core/fastapi_app/schemes.py
+++ core/fastapi_app/schemes.py
@@ -1,21 +1,54 @@
 import datetime
+from typing import Dict, Callable, Any, TypedDict, List
 
 from pydantic import BaseModel
 
-class Chat(BaseModel):
+UserId = int
+ChatId = int
+MessageId = int
+Count = int
+ActionName = str
+
+
+class ActionDTO(BaseModel):
+    """
+    Экземпляр действия, совершаемого при взаимодействии
+    с приложением через вебсокет
+
+    Формат: {name: str, {...тело}}
+    """
+    name: ActionName
+    body: dict
+
+
+class ChatDTO(BaseModel):
     """
     Экземпляр чата, пришедшего с главного сервера
     """
-    id: int
+    id: ChatId
     name: str
+
 
 class MessageDTO(BaseModel):
     """
     Экземпляр сообщения, в данном формате они хранятся
     на главном сервере
     """
-    id: int
-    chat_id: int
-    sender_id: int
+    id: MessageId
+    chat_id: ChatId
+    sender_id: UserId
     text: str
     date: datetime.datetime | str
+
+
+class UserDTO(BaseModel):
+    id: UserId
+    name: str
+
+
+class ActionsMapTypedDict(TypedDict):
+    get_waiting_chats: Callable[[UserId], List[ChatDTO]] | None
+    read_chat_by_user: Callable[[UserId, ChatId], ChatDTO] | None
+    get_chats_by_user: Callable[[UserId], List[ChatDTO]] | None
+    get_messages_from_chat: Callable[[ChatId, Count], List[MessageDTO]] | None
+    send_message_to_chat: Callable[[MessageDTO], None] | None

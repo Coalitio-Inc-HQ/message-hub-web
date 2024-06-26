2ed7fdc2e7ded40627169bb373eb10aa988e61e9
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-26 16:08:02 +0900

    WrongResponseFormatFromMainException

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,2 +1,10 @@
 class WrongBodyFormatException(Exception):
     pass
+
+class WrongResponseFormatFromMainException(Exception):
+    """
+    Пришел неверный формат из main.
+    Либо меняй формат у себя (нежелательно, так как со мной общается
+    ещё и фронт), либо уведоми ответственного за главный сервер
+    """
+    pass

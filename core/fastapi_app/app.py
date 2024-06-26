1a188aa8b8491bd62751626ee1a787876d251382
laga-vladislav <laga.ladislav@yandex.ru>
2024-06-26 16:47:02 +0900

    Исправил нейминг

--- core/fastapi_app/app.py
+++ core/fastapi_app/app.py
@@ -2,9 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from starlette.websockets import WebSocketDisconnect, WebSocket
 
-from pydantic import parse_obj_as
 
-from core.fastapi_app.front_client import (answer_front_waiting_chats_by_user,
+from core.fastapi_app.front_client import (answer_front_waiting_chats,
                                            answer_front_chats_by_user,
                                            answer_front_messages_from_chat,
                                            process_front_message_to_chat)
@@ -13,7 +12,7 @@ from core.fastapi_app.main_client import router
 from core import *
 
 actions_map = ActionsMapTypedDict(
-    get_waiting_chats=answer_front_waiting_chats_by_user,
+    get_waiting_chats=answer_front_waiting_chats,
     get_chats_by_user=answer_front_chats_by_user,
     get_messages_by_chat=answer_front_messages_from_chat,
     send_message_to_chat=process_front_message_to_chat

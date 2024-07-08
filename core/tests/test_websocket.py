ee576ceebbc546c75d834202bf5c96deaa1195b6
VladosIncorparated <podorvan03@mail.ru>
2024-07-08 12:34:30 +0900

    Внедрён pytest
    Внедрён pytest

--- core/tests/test_websocket.py
+++ core/tests/test_websocket.py
@@ -4,9 +4,14 @@ from core.fastapi_app.app import app
 
 from core import app_config, ActionDTO
 
+from httpx import AsyncClient
 
-def test_front_websocket_waiting_chats_by_user():
-    client = TestClient(app)
+import json
+
+async def test_front_websocket_waiting_chats_by_user(ac: AsyncClient):
+    await ac.post("/auth/register", json={"name": "test", "email": "test@test.py","password":"1234567890"})
+    response = await ac.post("/auth/jwt/login", data={"username": "test@test.py","password":"1234567890"})
+    client = TestClient(app, cookies={"fastapiusersauth":response.cookies.get("fastapiusersauth")})
     with client.websocket_connect(f"{app_config.INTERNAL_WS_LISTENER_PREFIX}") as websocket:
         send_action = ActionDTO(name="get_waiting_chats", body={"count": 12})
         websocket.send_json(send_action.dict())
@@ -14,8 +19,9 @@ def test_front_websocket_waiting_chats_by_user():
         print(data)
 
 
-def test_front_websocket_get_chats_by_user():
-    client = TestClient(app)
+async def test_front_websocket_get_chats_by_user(ac: AsyncClient):
+    response = await ac.post("/auth/jwt/login", data={"username": "test@test.py", "password": "1234567890"})
+    client = TestClient(app, cookies={"fastapiusersauth": response.cookies.get("fastapiusersauth")})
     with client.websocket_connect(f"{app_config.INTERNAL_WS_LISTENER_PREFIX}") as websocket:
         send_action = ActionDTO(name="get_chats_by_user", body={"user_id": 12})
         websocket.send_json(send_action.dict())
@@ -23,8 +29,9 @@ def test_front_websocket_get_chats_by_user():
         print(data)
 
 
-def test_front_websocket_get_messages_by_chat():
-    client = TestClient(app)
+async def test_front_websocket_get_messages_by_chat(ac: AsyncClient):
+    response = await ac.post("/auth/jwt/login", data={"username": "test@test.py", "password": "1234567890"})
+    client = TestClient(app, cookies={"fastapiusersauth": response.cookies.get("fastapiusersauth")})
     with client.websocket_connect(f"{app_config.INTERNAL_WS_LISTENER_PREFIX}") as websocket:
         send_action = ActionDTO(name="get_messages_by_chat",
                                 body={"chat_id": 1, 'count': 2, 'offset_message_id': -1})
@@ -33,8 +40,9 @@ def test_front_websocket_get_messages_by_chat():
         print(data)
 
 
-def test_front_websocket_send_message_to_chat():
-    client = TestClient(app)
+async def test_front_websocket_send_message_to_chat(ac: AsyncClient):
+    response = await ac.post("/auth/jwt/login", data={"username": "test@test.py", "password": "1234567890"})
+    client = TestClient(app, cookies={"fastapiusersauth": response.cookies.get("fastapiusersauth")})
     with client.websocket_connect(f"{app_config.INTERNAL_WS_LISTENER_PREFIX}") as websocket:
         send_action = ActionDTO(name="send_message_to_chat",
                                 body={

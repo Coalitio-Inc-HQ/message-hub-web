db9472e6f54f3547414204409cc85761abe6344e
laga-vladislav <123343007+laga-vladislav@users.noreply.github.com>
2024-07-22 17:15:36 +0900

    Убрал SETUP. Не знаю, зачем он был нужен, более того, выдавал warning
    Также добавил ASGITransport, чтобы убрать ещё один warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,18 @@
-import asyncio
 from typing import AsyncGenerator
 
 import pytest
-from fastapi.testclient import TestClient
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
 from database.database_engine import get_session
 from database.database_schemes import Base
-from core.config_reader import config
 from fastapi_app.app import app
+from core import app_config
 
 # DATABASE
-DATABASE_URL_TEST = f"{config.DB_TYPE}+{config.DB_DRIVER}:///./data/Test.db"
+DATABASE_URL_TEST = f"{app_config.DB_TYPE}+{app_config.DB_DRIVER}:///./data/Test.db"
 
 engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
 async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
@@ -38,19 +36,7 @@ async def prepare_database():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-# SETUP
-@pytest.fixture(scope='session')
-def event_loop(request):
-    """Create an instance of the default event loop for each test case."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
-client = TestClient(app)
-
-
 @pytest.fixture(scope="session")
 async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         yield ac

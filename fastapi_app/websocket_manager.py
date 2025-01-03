3034008a04284d7fc5bbbaff129b189120e5c282
VladosIncorparated <podorvan03@mail.ru>
2025-01-03 10:25:32 +0900

    ConnectionManager update учтино наличие нескольких соедений для одного пользователя

--- fastapi_app/websocket_manager.py
+++ fastapi_app/websocket_manager.py
@@ -10,18 +10,25 @@ class ConnectionManager:
     """
 
     def __init__(self):
-        self.active_connections: dict[WebSocket] = {}
+        self.active_connections: dict[list[WebSocket]] = {}
         self.active_chat_connections: dict[set[int]] = {}
 
     async def connect(self, websocket: WebSocket, user_id: int):
         await websocket.accept()
-        self.active_connections[user_id] = websocket
+        if self.active_connections[user_id]:
+            self.active_connections[user_id].append(websocket)
+        else:
+            self.active_connections[user_id] = [websocket]
 
     def disconnect(self, websocket: WebSocket, user_id: int):
-        # или найти по WebSocket, user_id
-        for chat_connections in self.active_chat_connections:
-            self.active_chat_connections[chat_connections].remove(user_id)
-        self.active_connections.pop(user_id)
+        self.active_connections[user_id].remove(websocket)
+
+        if len(self.active_connections[user_id]) == 0:
+            # или найти по WebSocket, user_id
+            for chat_connections in self.active_chat_connections:
+                self.active_chat_connections[chat_connections].remove(user_id)
+            self.active_connections.pop(user_id)
+
         print(self.active_chat_connections)
 
     async def connect_user_to_chats(self, user_id: int, chat_ids: list[int]):
@@ -34,11 +41,12 @@ class ConnectionManager:
         :return:
         """
 
-        for chat_id in chat_ids:
-            if chat_id in self.active_chat_connections:
-                self.active_chat_connections[chat_id].add(user_id)
-            else:
-                self.active_chat_connections[chat_id] = {user_id}
+        if user_id in self.active_connections:
+            for chat_id in chat_ids:
+                if chat_id in self.active_chat_connections:
+                    self.active_chat_connections[chat_id].add(user_id)
+                else:
+                    self.active_chat_connections[chat_id] = {user_id}
 
     async def connect_user_to_chat(self, user_id: int, chat_id: int):
         """
@@ -74,8 +82,9 @@ class ConnectionManager:
         :return:
         """
 
-        for connection in self.active_connections:
-            await self.active_connections[connection].send_json(action.model_dump())
+        for user_connection in self.active_connections:
+            for connection in user_connection:
+                await connection.send_json(action.model_dump())
 
     async def send_to_chat(self, action: ActionDTO, chat_id: int):
         """
@@ -87,9 +96,9 @@ class ConnectionManager:
         """
 
         if chat_id in self.active_chat_connections:
-            for connection_id in self.active_chat_connections[chat_id]:
-                print(connection_id)
-                await self.active_connections[connection_id].send_json(action.model_dump())
+            for user_connection_id in self.active_chat_connections[chat_id]:
+                for connection in self.active_connections[user_connection_id]:
+                    await connection.send_json(action.model_dump())
 
     async def send_to_user_by_user_id(self, action: ActionDTO, user_id: int):
         """
@@ -100,7 +109,7 @@ class ConnectionManager:
         :return:
         """
         if user_id in self.active_connections:
-            await self.active_connections[user_id].send_json(action.model_dump())
-
+            for connection in self.active_connections[user_id]:
+                await connection.send_json(action.model_dump())
 
 websocket_manager = ConnectionManager()
